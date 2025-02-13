445e38280b4245a57e848d8d05db206c1115494a
TreeFiddyBilly <logang17af@gmail.com>
2025-02-13 21:38:01 +0000

    Initial Commit

---
+++ currencyConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  currencyConverter
+//
+//  Created by user270532 on 2/13/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ currencyConverterTests/currencyConverterTests.swift
@@ -0,0 +1,17 @@
+//
+//  currencyConverterTests.swift
+//  currencyConverterTests
+//
+//  Created by user270532 on 2/13/25.
+//
+
+import Testing
+@testable import currencyConverter
+
+struct currencyConverterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
